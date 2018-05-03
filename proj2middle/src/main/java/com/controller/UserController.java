9db72ef70e196fa4b48b596a7a6a2598e567b552
Tusharpasrija <34834575+Tusharpasrija@users.noreply.github.com>
2018-05-03 15:39:15 +0530

    Add files via upload

---
+++ proj2middle/src/main/java/com/controller/UserController.java
@@ -0,0 +1,144 @@
+package com.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dao.UserDao;
+import com.model.ErrorClazz;
+import com.model.User;
+
+
+@RestController
+public class UserController {
+
+	@Autowired
+	private UserDao userDao;
+	
+	public UserController()
+	{
+		System.out.println("UserController bean is created");
+	}
+	
+	//?Any type[Error class/User object]
+	@RequestMapping(value="registeruser",method=RequestMethod.POST)
+	public ResponseEntity<?> registerUser(@RequestBody User user)
+	{
+	System.out.println("registerUser in UserController" + user);
+	if(!userDao.isEmailUnique(user.getEmail()))
+	{
+		ErrorClazz error=new ErrorClazz(1,"Email already exist..please enter different email address");
+		return new ResponseEntity<ErrorClazz>(error,HttpStatus.CONFLICT); 
+	}
+	
+	try
+	{
+	userDao.registerUser(user);
+	}
+	catch(Exception e)
+	{
+		ErrorClazz error=new ErrorClazz(2,"Some fields may be empty..");
+		return new ResponseEntity<ErrorClazz>(error,HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	return new ResponseEntity<User>(user,HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public ResponseEntity<?> login(@RequestBody User user , HttpSession session)
+	{
+		System.out.println(user);
+		User validUser=userDao.login(user);
+		System.out.println(validUser);
+		if(validUser==null)
+		{
+			ErrorClazz error=new ErrorClazz(5,"Login Failed..Invalid Email/Password");
+			return new ResponseEntity<ErrorClazz>(error,HttpStatus.UNAUTHORIZED);  
+		}
+		else
+		{
+			validUser.setOnline(true);
+			userDao.update(validUser);
+			session.setAttribute("loginId",user.getEmail());
+			return new ResponseEntity<User>(validUser,HttpStatus.OK);
+		}
+	}
+
+	@RequestMapping(value="/logout",method=RequestMethod.PUT)
+	public ResponseEntity<?> logout(HttpSession session){
+		String email=(String) session.getAttribute("loginId");
+		if(email==null)
+		{
+			ErrorClazz error=new ErrorClazz(4,"Please login..");
+			return new ResponseEntity<ErrorClazz>(error,HttpStatus.UNAUTHORIZED);  
+		}
+		User user=userDao.getUser(email);
+		user.setOnline(false);
+		userDao.update(user);
+		session.removeAttribute("loginId");
+		session.invalidate();
+		return new ResponseEntity<User>(user,HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/getuser",method=RequestMethod.GET)
+	public ResponseEntity<?> getUser(HttpSession session)
+	{
+		String email=(String) session.getAttribute("loginId");
+		if(email==null)
+		{
+			ErrorClazz error=new ErrorClazz(5,"Unauthorised Access..");
+			return new ResponseEntity<ErrorClazz>(error,HttpStatus.UNAUTHORIZED);  	
+		}
+		User user=userDao.getUser(email);
+		return new ResponseEntity<User>(user,HttpStatus.OK);
+		}
+	
+	@RequestMapping(value="/updateuser",method=RequestMethod.PUT)
+	public ResponseEntity<?> updateUser(@RequestBody User user,HttpSession session)
+	{
+		String email=(String) session.getAttribute("loginId");
+		if(email==null)//not logged in
+		{
+			ErrorClazz error=new ErrorClazz(5,"Unauthorised Access..");
+			return new ResponseEntity<ErrorClazz>(error,HttpStatus.UNAUTHORIZED);  	
+		}
+		try{
+		userDao.update(user);
+		return new ResponseEntity<User>(user,HttpStatus.OK);
+		}
+		catch(Exception e)
+		{
+			ErrorClazz error=new ErrorClazz(5,"Unable to update user details.."+e.getMessage());
+			return new ResponseEntity<ErrorClazz>(error,HttpStatus.INTERNAL_SERVER_ERROR); 
+		}
+
+	}
+	
+	@RequestMapping(value="/searchuser/{name}",method=RequestMethod.GET)
+	public ResponseEntity<?> searchUsers(@PathVariable String name,HttpSession session){
+		
+		String email=(String) session.getAttribute("loginId");
+		if(email==null)//not logged in
+		{
+			ErrorClazz error=new ErrorClazz(5,"Unauthorised Access..");
+			return new ResponseEntity<ErrorClazz>(error,HttpStatus.UNAUTHORIZED);  	
+		}
+		List<User> users=userDao.searchUser(name);
+		return new ResponseEntity<List<User>>(users,HttpStatus.OK);
+	}
+}
+
+
+
+
+
+
